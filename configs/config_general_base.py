730bf18697b90d53ca0d060b6d12b37778a76241
NilsDem <nils.demerle@gmail.com>
2025-08-19 10:45:43 +0200

    changes to export code

--- configs/config_general_base.py
+++ configs/config_general_base.py
@@ -3,7 +3,7 @@ batch_size = 8  # batch size
 lr = 0.0001  # learning rate
 total_iters = 1000000  # total iterations
 iters_per_epoch = 10000  # number of iterations approximately in every epoch
-compile_model = False  # compile the model for faster training (will require ~10 minutes of compilation time only on first run)
+compile_model = True  # compile the model for faster training (will require ~10 minutes of compilation time only on first run)
 num_workers = 16  # number of dataloader workers
 multi_gpu = False  # use DistributedDataParallel multi-gpu training, to be used with torchrun
 
